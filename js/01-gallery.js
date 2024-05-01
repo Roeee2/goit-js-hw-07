ce38534f81bcf54a48107297dee4f4edbd3b1b10
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-05-01 17:55:41 +0100

    task7-done-for-real

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
+// Change code below this line
 
 const gallery = document.querySelector(".gallery");
-
 for (const item of galleryItems) {
   gallery.insertAdjacentHTML(
     "beforeend",
